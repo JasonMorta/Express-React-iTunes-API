9f6b150edddbfc84bff7705e58147f1f28028396
JasonMorta <jason.morta23@gmail.com>
2022-06-26 13:26:20 +0200

    Update SearchFor.jsx

--- frontend/src/components/SearchFor.jsx
+++ frontend/src/components/SearchFor.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
-import Feedback from 'react-bootstrap/Feedback'
 
 export default function SearchFor(props) {
   let loadingBar = (
